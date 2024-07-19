c74c161bfd9a254a4b173a8ef37d783d58dfa2f9
PsydoV2 <seb.falter@gmail.com>
2024-07-19 14:57:17 +0200

    add

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import StartPage from "./components/StartPage";
 import { useEffect, useState } from "react";
 
+interface MovieProps {
+  poster: string;
+  Title: string;
+  type: string;
+  Year: string;
+  imdbID: string;
+}
+
 export default function App() {
   const key = "ba5bd1b3";
+  const [currentMovie, setCurrentMovie] = useState<MovieProps>();
+  const [nextMovie, setNextMovie] = useState<MovieProps>();
+
   function randomIntFromInterval(min: number, max: number) {
-    // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  function getMovie() {
-    const id = "tt" + randomIntFromInterval(11111111, 99999999);
-    fetch(`http://www.omdbapi.com/?apikey=${key}&i=${id}`)
+  function getCurrentMovie() {
+    const randomYear = randomIntFromInterval(1950, new Date().getFullYear());
+    console.log(randomYear);
+
+    fetch(
+      `https://www.omdbapi.com/?apikey=${key}&s=movie&y=${randomYear}&type=movie`
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -20,41 +40,72 @@ export default function App() {
         return response.json();
       })
       .then((data) => {
-        if (data.Response == "False") {
-          getMovie();
+        if (data.Response === "False") {
+          getCurrentMovie();
+        } else {
+          setCurrentMovie(data);
+          console.log(data);
+          getNextMovie();
         }
-        console.log(data);
       })
       .catch((error) => {
         console.error("Error:", error);
-        getMovie();
+        getCurrentMovie();
+      });
+  }
+
+  function getNextMovie() {
+    const randomYear = randomIntFromInterval(1950, new Date().getFullYear());
+    console.log(randomYear);
+
+    fetch(
+      `https://www.omdbapi.com/?apikey=${key}&s=movie&y=${randomYear}&type=movie`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.Response === "False") {
+          getNextMovie();
+        } else {
+          setNextMovie(data);
+          console.log(data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        getNextMovie();
       });
   }
 
   return (
     <View style={styles.main}>
-      <StartPage handelToggle={getMovie}></StartPage>
+      <StartPage handelToggle={() => getCurrentMovie()}></StartPage>
       <StatusBar style="light" />
 
       <View style={styles.topMovieCon}>
-        <Text style={styles.movieTitle}>"Movie Name"</Text>
+        {/* <ImageBackground source={currentMovie?.poster}></ImageBackground> */}
+        <Text style={styles.movieTitle}>{currentMovie?.Title}</Text>
         <Text>was released</Text>
-        <Text>12.2.2222</Text>
+        <Text>{currentMovie?.Year}</Text>
       </View>
 
       <View style={styles.divider}></View>
       <Text style={styles.dividerText}>VS</Text>
 
       <View style={styles.bottomMovieCon}>
-        <Text style={styles.movieTitle}>"Movie Name"</Text>
+        <Text style={styles.movieTitle}>{nextMovie?.Title}</Text>
         <Text>was</Text>
         <TouchableOpacity style={styles.buttonHigher}>
-          <Text style={styles.buttonText}>Higher</Text>
+          <Text style={styles.buttonText}>Before</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttonLower}>
-          <Text style={styles.buttonText}>Lower</Text>
+          <Text style={styles.buttonText}>After</Text>
         </TouchableOpacity>
-        <Text>12.2.2222 released</Text>
+        <Text>{currentMovie?.Year} released</Text>
       </View>
     </View>
   );
@@ -104,7 +155,6 @@ const styles = StyleSheet.create({
     width: 50,
     borderRadius: 50,
     fontSize: 30,
-
     textAlign: "center",
     lineHeight: 50,
     fontWeight: "900",
